6eebfc592e0709010122ecfd79d6edffb7a8fe26
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-08-21 09:21:03 -0300

    Adicionando toString

--- src/curso/java/executavel/PrimeiraClasseJava.java
+++ src/curso/java/executavel/PrimeiraClasseJava.java
@@ -43,16 +43,12 @@ public class PrimeiraClasseJava {
         aluno1.setNota3(Double.parseDouble(Nota3));
         aluno1.setNota4(Double.parseDouble(Nota4));
 
-        
-		System.out.println("Nome é = " + aluno1.getNome());
-		System.out.println("Idade é = " + aluno1.getIdade());
-		System.out.println("Nascimento é = " + aluno1.getDataNascimento());
-		System.out.println("Média da nota é: " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ?  "Aprovado" : "Reprovado"));
-		System.out.println("resultado 2 = " + aluno1.getAlunoAprovado2());
-		
-		
-		
+	
+    	System.out.println(aluno1.toString());   /*Descrição do objeto na memoria*/
+		System.out.println("Média do aluno = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
+
+
 
 	}
 
