582eba71a15cbd736d258a797b5028473855d6c4
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-08-22 15:16:50 -0300

    Atributos de disciplina

--- src/curso/java/classes/Aluno.java
+++ src/curso/java/classes/Aluno.java
@@ -15,11 +15,49 @@ public class Aluno {
 		private String nomeEscola;
 		private String serieMatriculado;
 		private double nota1;
+		private String disciplina1;
 		private double nota2;
+		private String disciplina2;
 		private double nota3;
+		private String disciplina3;
 		private double nota4;
+		private String disciplina4;
+	
 		
 		
+		
+		public String getDisciplina1() {
+			return disciplina1;
+		}
+
+		public void setDisciplina1(String disciplina1) {
+			this.disciplina1 = disciplina1;
+		}
+
+		public String getDisciplina2() {
+			return disciplina2;
+		}
+
+		public void setDisciplina2(String disciplina2) {
+			this.disciplina2 = disciplina2;
+		}
+
+		public String getDisciplina3() {
+			return disciplina3;
+		}
+
+		public void setDisciplina3(String disciplina3) {
+			this.disciplina3 = disciplina3;
+		}
+
+		public String getDisciplina4() {
+			return disciplina4;
+		}
+
+		public void setDisciplina4(String disciplina4) {
+			this.disciplina4 = disciplina4;
+		}
+
 		public double getNota1() {
 			return nota1;
 		}
@@ -165,13 +203,17 @@ public class Aluno {
 			 }
 			}
 
+	
+
 		@Override
 		public String toString() {
 			return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento
 					+ ", registroGeral=" + registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae
 					+ ", nomePai=" + nomePai + ", dataMatricula=" + dataMatricula + ", nomeMatricula=" + nomeMatricula
 					+ ", nomeEscola=" + nomeEscola + ", serieMatriculado=" + serieMatriculado + ", nota1=" + nota1
-					+ ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4 + "]";
+					+ ", disciplina1=" + disciplina1 + ", nota2=" + nota2 + ", disciplina2=" + disciplina2 + ", nota3="
+					+ nota3 + ", disciplina3=" + disciplina3 + ", nota4=" + nota4 + ", disciplina4=" + disciplina4
+					+ "]";
 		}
 
 		@Override
--- src/curso/java/executavel/PrimeiraClasseJava.java
+++ src/curso/java/executavel/PrimeiraClasseJava.java
@@ -8,8 +8,7 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-  
-		/*String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual a idade?");
 		String dataNascumento = JOptionPane.showInputDialog("Qual a data de nascimento?");
 		String registroGeral = JOptionPane.showInputDialog("Registro Geral?");
@@ -20,13 +19,21 @@ public class PrimeiraClasseJava {
 		String NomeMatricula = JOptionPane.showInputDialog("Qual é o nome da Matricula?");
 		String SerieMatriculado = JOptionPane.showInputDialog("Qual a serie matriculada?");
 		String NomeEscola = JOptionPane.showInputDialog("Qual é o nome da escola?");
-		String Nota1 = JOptionPane.showInputDialog("Qual é a nota 1?");
-		String Nota2 = JOptionPane.showInputDialog("Qual é a nota 2?");
-		String Nota3 = JOptionPane.showInputDialog("Qual é a nota 3?");
-		String Nota4 = JOptionPane.showInputDialog("Qual é a nota 4?");
-
-		Aluno aluno1 = new Aluno();	
 		
+		String Nota1 = JOptionPane.showInputDialog("Qual é a nota 1?");
+		String disciplina1 = JOptionPane.showInputDialog("Disciplina1");
+		
+		String Nota2 = JOptionPane.showInputDialog("Qual é a nota 2?");
+		String disciplina2 = JOptionPane.showInputDialog("Disciplina2");
+		
+		String Nota3 = JOptionPane.showInputDialog("Qual é a nota 3?");
+		String disciplina3 = JOptionPane.showInputDialog("Disciplina3");
+		
+		String Nota4 = JOptionPane.showInputDialog("Qual é a nota 4?");
+		String disciplina4 = JOptionPane.showInputDialog("Disciplina4");
+
+		Aluno aluno1 = new Aluno();
+
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascumento);
@@ -37,34 +44,21 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula(DataMatricula);
 		aluno1.setNomeMatricula(NomeMatricula);
 		aluno1.setSerieMatriculado(SerieMatriculado);
-        aluno1.setNomeEscola(NomeEscola);
-        aluno1.setNota1(Double.parseDouble(Nota1));
-        aluno1.setNota2(Double.parseDouble(Nota2));
-        aluno1.setNota3(Double.parseDouble(Nota3));
-        aluno1.setNota4(Double.parseDouble(Nota4)); */
-
-	
-    	/*System.out.println(aluno1.toString());   /*Descrição do objeto na memoria*/
-		/*System.out.println("Média do aluno = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());*/
-
-		/*Equals e hashcode (Diferenciar objetos)*/
+		aluno1.setNomeEscola(NomeEscola);
 		
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Jhonatan");
-		aluno1.setNumeroCpf("123");
-
-		Aluno aluno2 = new Aluno();
-		aluno2.setNome("Jhonatan");
-		aluno2.setNumeroCpf("123");
-
+		aluno1.setNota1(Double.parseDouble(Nota1));
+		aluno1.setNota2(Double.parseDouble(Nota2));
+		aluno1.setNota3(Double.parseDouble(Nota3));
+		aluno1.setNota4(Double.parseDouble(Nota4));
 		
-		if (aluno1.equals(aluno2)) {
-			System.out.println("Alunos são iguais");						
-		}else {
-			System.out.println("Alunos não são iguais");						
+		aluno1.setDisciplina1(disciplina1);
+		aluno1.setDisciplina2(disciplina2);
+		aluno1.setDisciplina3(disciplina3);
+		aluno1.setDisciplina4(disciplina4);
 
-		}
+		System.out.println(aluno1.toString()); /* Descrição do objeto na memoria */
+		System.out.println("Média do aluno = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
 
 	}
 
