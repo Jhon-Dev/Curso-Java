c2f78d7efdf0f9ae4ef9965f30b4505ec3710856
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-09-03 01:09:38 -0300

    recuperando valores mo Array

--- src/curso/java/executavel/ArrayVetor.java
+++ src/curso/java/executavel/ArrayVetor.java
@@ -7,7 +7,7 @@ public class ArrayVetor {
 		/*Array pode ser de todos os tipos de dados e objetos também*/
 
 		/*Array sempre deve ter a quantidade de posições definidas*/
-		double notas [] = new double [3];
+		double notas [] = new double [4];
 		
 		notas[0] = 7.8;
 		notas[1] = 8.7; 
@@ -15,7 +15,15 @@ public class ArrayVetor {
 		notas [3] = 9.9;
 				
 
-		System.out.println("notas");
+		System.out.println("Nota 1 = " + notas[0]);
+		System.out.println("Nota 2 = " + notas[1]);
+		System.out.println("Nota 3 = " + notas[2]);
+		System.out.println("Nota 4 = " + notas[3]);
+		
+		for (int n = 0; n < 4; n++) {
+			System.out.println("Nota " + (n+1) + " é = " + notas[n]);
+		}
+
 	}
 
 }
