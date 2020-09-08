32a3453ef15ee951317a3a8ac630c266456f5793
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-09-07 23:55:36 -0300

    Ajustes

--- src/curso/java/executavel/PrimeiraClasseJava.java
+++ src/curso/java/executavel/PrimeiraClasseJava.java
@@ -27,8 +27,6 @@ public class PrimeiraClasseJava {
 
 			if (new FuncaoAutenticao(new Diretor(login, senha)).autenticar()) {
 
-
-
 				List<Aluno> alunos = new ArrayList<Aluno>();
 
 				HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
@@ -38,32 +36,30 @@ public class PrimeiraClasseJava {
 					String nome = JOptionPane.showInputDialog("Qual o nome do aluno " + qtd + "?");
 					String idade = JOptionPane.showInputDialog("Qual a idade?");
 
-					/*
-					 * dataNascumento = JOptionPane.showInputDialog("Qual a data de nascimento?");
-					 * String registroGeral = JOptionPane.showInputDialog("Registro Geral?"); String
-					 * cpf = JOptionPane.showInputDialog("Qual é o cpf?"); String NomeMae =
-					 * JOptionPane.showInputDialog("Qual é o nome da mãe?"); String NomePai =
-					 * JOptionPane.showInputDialog("Qual é o nome do Pai?"); String DataMatricula =
-					 * JOptionPane.showInputDialog("Qual é a matricula?"); String NomeMatricula =
-					 * JOptionPane.showInputDialog("Qual é o nome da Matricula?"); String
-					 * SerieMatriculado = JOptionPane.showInputDialog("Qual a serie matriculada?");
-					 * String NomeEscola = JOptionPane.showInputDialog("Qual é o nome da escola?");
-					 */
+					String dataNascumento = JOptionPane.showInputDialog("Qual a data de nascimento?");
+					String registroGeral = JOptionPane.showInputDialog("Registro Geral?");
+					String cpf = JOptionPane.showInputDialog("Qual é o cpf?");
+					String NomeMae = JOptionPane.showInputDialog("Qual é o nome da mãe?");
+					String NomePai = JOptionPane.showInputDialog("Qual é o nome do Pai?");
+					String DataMatricula = JOptionPane.showInputDialog("Qual é a matricula?");
+					String NomeMatricula = JOptionPane.showInputDialog("Qual é o nome da Matricula?");
+					String SerieMatriculado = JOptionPane.showInputDialog("Qual a serie matriculada?");
+					String NomeEscola = JOptionPane.showInputDialog("Qual é o nome da escola?");
 
 					Aluno aluno1 = new Aluno();
 
 					aluno1.setNome(nome);
 					aluno1.setIdade(Integer.valueOf(idade));
 
-					/*
-					 * aluno1.setDataNascimento(dataNascumento);
-					 * aluno1.setRegistroGeral(registroGeral); aluno1.setNumeroCpf(cpf);
-					 * aluno1.setNomeMae(NomeMae); aluno1.setNomePai(NomePai);
-					 * aluno1.setDataMatricula(DataMatricula);
-					 * aluno1.setNomeMatricula(NomeMatricula);
-					 * aluno1.setSerieMatriculado(SerieMatriculado);
-					 * aluno1.setNomeEscola(NomeEscola);
-					 */
+					aluno1.setDataNascimento(dataNascumento);
+					aluno1.setRegistroGeral(registroGeral);
+					aluno1.setNumeroCpf(cpf);
+					aluno1.setNomeMae(NomeMae);
+					aluno1.setNomePai(NomePai);
+					aluno1.setDataMatricula(DataMatricula);
+					aluno1.setNomeMatricula(NomeMatricula);
+					aluno1.setSerieMatriculado(SerieMatriculado);
+					aluno1.setNomeEscola(NomeEscola);
 
 					for (int pos = 1; pos <= 1; pos++) {
 						String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos + "?");
@@ -71,7 +67,7 @@ public class PrimeiraClasseJava {
 
 						Disciplina disciplina = new Disciplina();
 						disciplina.setDisciplina(nomeDisciplina);
-						//disciplina.setNota(Double.valueOf(nota));
+						// disciplina.setNota(Double.valueOf(nota));
 
 						aluno1.getDisciplinas().add(disciplina);
 					}
