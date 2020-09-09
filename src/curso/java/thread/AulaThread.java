40b06e6eb8ad9487a4475b679e3d47cb8bd6635c
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-09-09 01:25:30 -0300

    Processamento concorrente entre duas Thread

--- src/curso/java/thread/AulaThread.java
+++ src/curso/java/thread/AulaThread.java
@@ -6,7 +6,7 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/*Thread processando em paralelo*/
+		/*Thread processando em paralelo do envio do email*/
 		new Thread() {
 			
 			public void run() { /*Executa oque nós queremos*/
@@ -19,7 +19,7 @@ public class AulaThread {
 					System.out.println("Executando algo na rotina, por exemplo envio de e-mail");
 					
 					try {
-						Thread.sleep(1000);
+						Thread.sleep(2000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 						
@@ -32,7 +32,34 @@ public class AulaThread {
 			
 		}.start();  /* Liga a Thread que fica processando palelamente por trás*/
 		
+	/*==========================DIVISÃO DAS THREADS*/
 		
+		/*Thread processando em paralelo do envio de nota fical*/
+		new Thread() {
+			
+			public void run() { /*Executa oque nós queremos*/
+				/*Código da rotina*/		
+				
+				/*Código da rotina que eu quero executar em paralelo*/
+				for (int pos = 0; pos < 10; pos++) {
+
+					/* Quero executar esse envio com um tempo de parada, ou com tempo determinado */
+					System.out.println("Executando algo na rotina, por exemplo envio de nota fiscal");
+					
+					try {
+						Thread.sleep(4000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						
+					} /*Dá um intervalo de tempo*/
+				}
+				/*Fim do codigo pararelo*/
+				
+				
+			};
+			
+		}.start();  /* Liga a Thread que fica processando palelamente por trás*/
+
 		
 		/*Código do sistema do usuário continua o fluxo de trabalho*/
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE THREAD");		
