6adea9835eb21a77a5ac99ee260455a70eb9c2d8
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-09-10 00:49:43 -0300

    Conhecendo o GrideBagContraints gerenciador de posicionamento e layout

--- src/curso/java/thread/TelaTimeThread.java
+++ src/curso/java/thread/TelaTimeThread.java
@@ -1,10 +1,29 @@
 package curso.java.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.plaf.DimensionUIResource;
 
 public class TelaTimeThread extends JDialog{
+	
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout());  /*Painel de componentes*/
+	
+	private JLabel descricaoHora = new JLabel("Time da Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time da Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
+
+	
 
 	public TelaTimeThread() { /*Executa oque tiver dentro no momento da abertura, ou execução*/
 
@@ -14,9 +33,29 @@ public class TelaTimeThread extends JDialog{
 		setResizable(false);
 		/*Primeira parte concluida*/
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /*Controlador de posição de compondentes*/
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		
+		descricaoHora.setPreferredSize(new DimensionUIResource(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);
 		
 		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
 		
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+
+		
+		add(jPanel, BorderLayout.WEST);	
 		/*Sempre será o ultimo comando*/
 		setVisible(true); /*Torna a tela visivel para o usuário*/
 	}
